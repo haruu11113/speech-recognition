1a6929a1c31d0354085a117c98692642e909015f
haruu11113 <haruu11113@gmail.com>
2021-12-11 20:54:12 +0900

    feat: posenet

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ loaclhost:8080
 ## ref
 - []https://qiita.com/mktshhr/items/ac6351dd056e571715e0
 - []http://localhost:8080/yoro-index.html
+- []https://github.com/Hiroshi32Yoshida/posenet_flag_sign_js
---
+++ posenet/posenet_flag_sem.js
@@ -0,0 +1,528 @@
+const stats = new Stats();
+
+let genkaku = -1;
+let count = 0;
+let seq_genkaku = [];
+let curText = '';
+let score = 0;
+let bpface = new Image();
+let navScale = 1
+bpface.src = "bp_face.png"
+
+async function setupCamera() {
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    throw new Error(
+        'Browser API navigator.mediaDevices.getUserMedia not available');
+  }
+
+  const video = document.getElementById('video');
+  video.width = videoWidth;
+  video.height = videoHeight;
+
+  const mobile = isMobile();
+  const stream = await navigator.mediaDevices.getUserMedia({
+    'audio': false,
+    'video': {
+      facingMode: 'user',
+      width: mobile ? undefined : videoWidth,
+      height: mobile ? undefined : videoHeight,
+    },
+  });
+  video.srcObject = stream;
+
+  return new Promise((resolve) => {
+    video.onloadedmetadata = () => {
+      resolve(video);
+    };
+  });
+}
+
+async function loadVideo() {
+  const video = await setupCamera();
+  video.play();
+
+  return video;
+}
+
+const defaultQuantBytes = 2;
+
+const defaultMobileNetMultiplier = isMobile() ? 0.50 : 0.75;
+const defaultMobileNetStride = 16;
+const defaultMobileNetInputResolution = 500;
+
+const defaultResNetMultiplier = 1.0;
+const defaultResNetStride = 32;
+const defaultResNetInputResolution = 250;
+
+const guiState = {
+  algorithm: 'single-pose',
+  input: {
+    architecture: 'MobileNetV1',
+    outputStride: defaultMobileNetStride,
+    inputResolution: defaultMobileNetInputResolution,
+    multiplier: defaultMobileNetMultiplier,
+    quantBytes: defaultQuantBytes
+  },
+  singlePoseDetection: {
+    minPoseConfidence: 0.1,
+    minPartConfidence: 0.5,
+  },
+  multiPoseDetection: {
+    maxPoseDetections: 5,
+    minPoseConfidence: 0.15,
+    minPartConfidence: 0.1,
+    nmsRadius: 30.0,
+  },
+  output: {
+    showVideo: true,
+    showSkeleton: true,
+    showPoints: true,
+    showBoundingBox: false,
+    showBP: false,
+    showDebugText: false,
+  },
+  net: null,
+};
+
+/**
+ * Sets up dat.gui controller on the top-right of the window
+ */
+function setupGui(cameras, net) {
+  guiState.net = net;
+
+  if (cameras.length > 0) {
+    guiState.camera = cameras[0].deviceId;
+  }
+
+  const gui = new dat.GUI({width: 300});
+
+  let architectureController = null;
+  guiState[tryResNetButtonName] = function() {
+    architectureController.setValue('ResNet50')
+  };
+  gui.add(guiState, tryResNetButtonName).name(tryResNetButtonText);
+  updateTryResNetButtonDatGuiCss();
+
+  const algorithmController =
+      gui.add(guiState, 'algorithm', ['single-pose', 'multi-pose']);
+
+  let input = gui.addFolder('Input');
+
+  architectureController =
+      input.add(guiState.input, 'architecture', ['MobileNetV1', 'ResNet50']);
+  guiState.architecture = guiState.input.architecture;
+
+  let inputResolutionController = null;
+  function updateGuiInputResolution(
+      inputResolution,
+      inputResolutionArray,
+  ) {
+    if (inputResolutionController) {
+      inputResolutionController.remove();
+    }
+    guiState.inputResolution = inputResolution;
+    guiState.input.inputResolution = inputResolution;
+    inputResolutionController =
+        input.add(guiState.input, 'inputResolution', inputResolutionArray);
+    inputResolutionController.onChange(function(inputResolution) {
+      guiState.changeToInputResolution = inputResolution;
+    });
+  }
+
+  let outputStrideController = null;
+  function updateGuiOutputStride(outputStride, outputStrideArray) {
+    if (outputStrideController) {
+      outputStrideController.remove();
+    }
+    guiState.outputStride = outputStride;
+    guiState.input.outputStride = outputStride;
+    outputStrideController =
+        input.add(guiState.input, 'outputStride', outputStrideArray);
+    outputStrideController.onChange(function(outputStride) {
+      guiState.changeToOutputStride = outputStride;
+    });
+  }
+
+  let multiplierController = null;
+  function updateGuiMultiplier(multiplier, multiplierArray) {
+    if (multiplierController) {
+      multiplierController.remove();
+    }
+    guiState.multiplier = multiplier;
+    guiState.input.multiplier = multiplier;
+    multiplierController =
+        input.add(guiState.input, 'multiplier', multiplierArray);
+    multiplierController.onChange(function(multiplier) {
+      guiState.changeToMultiplier = multiplier;
+    });
+  }
+
+  let quantBytesController = null;
+  function updateGuiQuantBytes(quantBytes, quantBytesArray) {
+    if (quantBytesController) {
+      quantBytesController.remove();
+    }
+    guiState.quantBytes = +quantBytes;
+    guiState.input.quantBytes = +quantBytes;
+    quantBytesController =
+        input.add(guiState.input, 'quantBytes', quantBytesArray);
+    quantBytesController.onChange(function(quantBytes) {
+      guiState.changeToQuantBytes = +quantBytes;
+    });
+  }
+
+  function updateGui() {
+    if (guiState.input.architecture === 'MobileNetV1') {
+      updateGuiInputResolution(
+          defaultMobileNetInputResolution,
+          [200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800]);
+      updateGuiOutputStride(defaultMobileNetStride, [8, 16]);
+      updateGuiMultiplier(defaultMobileNetMultiplier, [0.50, 0.75, 1.0]);
+    } else {  // guiState.input.architecture === "ResNet50"
+      updateGuiInputResolution(
+          defaultResNetInputResolution,
+          [200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800]);
+      updateGuiOutputStride(defaultResNetStride, [32, 16]);
+      updateGuiMultiplier(defaultResNetMultiplier, [1.0]);
+    }
+    updateGuiQuantBytes(defaultQuantBytes, [1, 2, 4]);
+  }
+
+  updateGui();
+  //input.open();
+  let single = gui.addFolder('Single Pose Detection');
+  single.add(guiState.singlePoseDetection, 'minPoseConfidence', 0.0, 1.0);
+  single.add(guiState.singlePoseDetection, 'minPartConfidence', 0.0, 1.0);
+  single.open();
+
+  let multi = gui.addFolder('Multi Pose Detection');
+  multi.add(guiState.multiPoseDetection, 'maxPoseDetections')
+      .min(1)
+      .max(20)
+      .step(1);
+  multi.add(guiState.multiPoseDetection, 'minPoseConfidence', 0.0, 1.0);
+  multi.add(guiState.multiPoseDetection, 'minPartConfidence', 0.0, 1.0);
+  // nms Radius: controls the minimum distance between poses that are returned
+  // defaults to 20, which is probably fine for most use cases
+  multi.add(guiState.multiPoseDetection, 'nmsRadius').min(0.0).max(40.0);
+  //multi.open();
+
+  let output = gui.addFolder('Output');
+  output.add(guiState.output, 'showVideo');
+  output.add(guiState.output, 'showSkeleton');
+  output.add(guiState.output, 'showPoints');
+  output.add(guiState.output, 'showBoundingBox');
+  output.add(guiState.output, 'showBP');
+  output.add(guiState.output, 'showDebugText');
+  output.open();
+
+
+  architectureController.onChange(function(architecture) {
+    // if architecture is ResNet50, then show ResNet50 options
+    updateGui();
+    guiState.changeToArchitecture = architecture;
+  });
+
+  algorithmController.onChange(function(value) {
+    switch (guiState.algorithm) {
+      case 'single-pose':
+        multi.close();
+        single.open();
+        break;
+      case 'multi-pose':
+        single.close();
+        multi.open();
+        break;
+    }
+  });
+}
+
+/**
+ * Sets up a frames per second panel on the top-left of the window
+ */
+function setupFPS() {
+  stats.showPanel(0);  // 0: fps, 1: ms, 2: mb, 3+: custom
+  document.getElementById('main').appendChild(stats.dom);
+}
+
+function detectPoseInRealTime(video, net) {
+  const canvas = document.getElementById('output');
+  const ctx = canvas.getContext('2d');
+
+  const flipPoseHorizontal = false;
+
+  canvas.width = videoWidth;
+  canvas.height = videoHeight;
+
+  async function poseDetectionFrame() {
+    if (guiState.changeToArchitecture) {
+      // Important to purge variables and free up GPU memory
+      guiState.net.dispose();
+      toggleLoadingUI(true);
+      guiState.net = await posenet.load({
+        architecture: guiState.changeToArchitecture,
+        outputStride: guiState.outputStride,
+        inputResolution: guiState.inputResolution,
+        multiplier: guiState.multiplier,
+      });
+      toggleLoadingUI(false);
+      guiState.architecture = guiState.changeToArchitecture;
+      guiState.changeToArchitecture = null;
+    }
+
+    if (guiState.changeToMultiplier) {
+      guiState.net.dispose();
+      toggleLoadingUI(true);
+      guiState.net = await posenet.load({
+        architecture: guiState.architecture,
+        outputStride: guiState.outputStride,
+        inputResolution: guiState.inputResolution,
+        multiplier: +guiState.changeToMultiplier,
+        quantBytes: guiState.quantBytes
+      });
+      toggleLoadingUI(false);
+      guiState.multiplier = +guiState.changeToMultiplier;
+      guiState.changeToMultiplier = null;
+    }
+
+    if (guiState.changeToOutputStride) {
+      // Important to purge variables and free up GPU memory
+      guiState.net.dispose();
+      toggleLoadingUI(true);
+      guiState.net = await posenet.load({
+        architecture: guiState.architecture,
+        outputStride: +guiState.changeToOutputStride,
+        inputResolution: guiState.inputResolution,
+        multiplier: guiState.multiplier,
+        quantBytes: guiState.quantBytes
+      });
+      toggleLoadingUI(false);
+      guiState.outputStride = +guiState.changeToOutputStride;
+      guiState.changeToOutputStride = null;
+    }
+
+    if (guiState.changeToInputResolution) {
+      // Important to purge variables and free up GPU memory
+      guiState.net.dispose();
+      toggleLoadingUI(true);
+      guiState.net = await posenet.load({
+        architecture: guiState.architecture,
+        outputStride: guiState.outputStride,
+        inputResolution: +guiState.changeToInputResolution,
+        multiplier: guiState.multiplier,
+        quantBytes: guiState.quantBytes
+      });
+      toggleLoadingUI(false);
+      guiState.inputResolution = +guiState.changeToInputResolution;
+      guiState.changeToInputResolution = null;
+    }
+
+    if (guiState.changeToQuantBytes) {
+      // Important to purge variables and free up GPU memory
+      guiState.net.dispose();
+      toggleLoadingUI(true);
+      guiState.net = await posenet.load({
+        architecture: guiState.architecture,
+        outputStride: guiState.outputStride,
+        inputResolution: guiState.inputResolution,
+        multiplier: guiState.multiplier,
+        quantBytes: guiState.changeToQuantBytes
+      });
+      toggleLoadingUI(false);
+      guiState.quantBytes = guiState.changeToQuantBytes;
+      guiState.changeToQuantBytes = null;
+    }
+
+    // Begin monitoring code for frames per second
+    stats.begin();
+
+    let poses = [];
+    let minPoseConfidence;
+    let minPartConfidence;
+    switch (guiState.algorithm) {
+      case 'single-pose':
+        const pose = await guiState.net.estimatePoses(video, {
+          flipHorizontal: flipPoseHorizontal,
+          decodingMethod: 'single-person'
+        });
+        poses = poses.concat(pose);
+        minPoseConfidence = +guiState.singlePoseDetection.minPoseConfidence;
+        minPartConfidence = +guiState.singlePoseDetection.minPartConfidence;
+        break;
+      case 'multi-pose':
+        let all_poses = await guiState.net.estimatePoses(video, {
+          flipHorizontal: flipPoseHorizontal,
+          decodingMethod: 'multi-person',
+          maxDetections: guiState.multiPoseDetection.maxPoseDetections,
+          scoreThreshold: guiState.multiPoseDetection.minPartConfidence,
+          nmsRadius: guiState.multiPoseDetection.nmsRadius
+        });
+
+        poses = poses.concat(all_poses);
+        minPoseConfidence = +guiState.multiPoseDetection.minPoseConfidence;
+        minPartConfidence = +guiState.multiPoseDetection.minPartConfidence;
+        break;
+    }
+
+    ctx.clearRect(0, 0, videoWidth, videoHeight);
+
+    if (guiState.output.showVideo) {
+      ctx.save();
+      //ctx.scale(-1, 1);
+      //ctx.translate(-videoWidth, 0);
+      ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+      ctx.restore();
+    }
+
+    let exwidth = (videoWidth > videoHeight)? videoWidth: videoHeight;
+    let genkakupoint = Math.floor(exwidth/4);
+    let genkakusize = Math.floor(exwidth/3);
+    let strsize = Math.floor(exwidth/10);
+
+    poses.forEach(({score, keypoints}) => {
+      if (score >= minPoseConfidence) {
+        if (guiState.output.showPoints) {
+          drawKeypoints(keypoints, minPartConfidence, ctx);
+        }
+        if (guiState.output.showSkeleton) {
+          drawSkeleton(keypoints, minPartConfidence, ctx);
+        }
+        if (guiState.output.showBoundingBox) {
+          drawBoundingBox(keypoints, ctx);
+        }
+        if (guiState.output.showBP){
+          drawBP(keypoints[NOSE], keypoints[LEFTEYE], ctx);
+        }
+        if (guiState.output.showDebugText){
+          drawDebugText(keypoints, minPartConfidence, "blue", ctx);
+        }
+      }
+
+      result = judge_genkaku(keypoints, minPartConfidence);
+      if(result != -1){
+          if(genkaku == result){
+              count++;
+              if(count == 5){
+                  seq_genkaku.push(genkaku);
+              }
+          }
+          else{
+              genkaku = result;
+              count = 0;
+          }
+
+          if( result == 0){
+              curText = judge_kana(curText, seq_genkaku);
+              seq_genkaku.splice(0);
+          }
+
+          if(count > 4){
+              ctx.font = "bold " + genkakusize.toString() + "px sans-serif";
+              ctx.fillStyle = "white";
+              ctx.fillText(genkaku, genkakupoint, strsize+20+genkakusize);
+          }
+      }
+
+      // draw strings
+      ctx.font = "bold " + strsize.toString() + "px sans-serif";
+      ctx.fillStyle = "red";
+      ctx.fillText(curText, 10, strsize + 5);
+      ctx.fill();
+
+    });
+
+    // End monitoring code for frames per second
+    stats.end();
+
+    requestAnimationFrame(poseDetectionFrame);
+  }
+
+  poseDetectionFrame();
+}
+
+async function bindPage() {
+  toggleLoadingUI(true);
+  const net = await posenet.load({
+    architecture: guiState.input.architecture,
+    outputStride: guiState.input.outputStride,
+    inputResolution: guiState.input.inputResolution,
+    multiplier: guiState.input.multiplier,
+    quantBytes: guiState.input.quantBytes
+  });
+  toggleLoadingUI(false);
+
+  let video;
+
+  try {
+    video = await loadVideo();
+  } catch (e) {
+    let info = document.getElementById('info');
+    info.textContent = 'this browser does not support video capture,' +
+        'or this device does not have a camera';
+    info.style.display = 'block';
+    throw e;
+  }
+
+  setupGui([], net);
+  //setupFPS();
+  detectPoseInRealTime(video, net);
+}
+
+navigator.getUserMedia = navigator.getUserMedia ||
+    navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+bindPage();
+
+const color = 'aqua';
+const boundingBoxColor = 'red';
+const lineWidth = 2;
+
+const tryResNetButtonName = 'tryResNetButton';
+const tryResNetButtonText = '[New] Try ResNet50';
+const tryResNetButtonTextCss = 'width:100%;text-decoration:underline;';
+const tryResNetButtonBackgroundCss = 'background:#e61d5f;';
+
+function setDatGuiPropertyCss(propertyText, liCssString, spanCssString = '') {
+  var spans = document.getElementsByClassName('property-name');
+  for (var i = 0; i < spans.length; i++) {
+    var text = spans[i].textContent || spans[i].innerText;
+    if (text == propertyText) {
+      spans[i].parentNode.parentNode.style = liCssString;
+      if (spanCssString !== '') {
+        spans[i].style = spanCssString;
+      }
+    }
+  }
+}
+
+function updateTryResNetButtonDatGuiCss() {
+  setDatGuiPropertyCss(
+      tryResNetButtonText, tryResNetButtonBackgroundCss,
+      tryResNetButtonTextCss);
+}
+
+/**
+ * Toggles between the loading UI and the main canvas UI.
+ */
+function toggleLoadingUI(
+    showLoadingUI, loadingDivId = 'loading', mainDivId = 'main') {
+  if (showLoadingUI) {
+    document.getElementById(loadingDivId).style.display = 'block';
+    document.getElementById(mainDivId).style.display = 'none';
+  } else {
+    document.getElementById(loadingDivId).style.display = 'none';
+    document.getElementById(mainDivId).style.display = 'block';
+  }
+}
+
+function toTuple({y, x}) {
+  return [y, x];
+}
+
+function drawBP(nose, leye, ctx){
+  navScale = (leye.position.x - nose.position.x - 50) / 20;
+  if (navScale < 1) navScale = 1;
+  let nw = bpface.width * navScale;
+  let nh = bpface.height * navScale;
+  ctx.drawImage(bpface,nose.position.x - nw / 2 , nose.position.y - nh / 1.5, nw, nh);
+}
